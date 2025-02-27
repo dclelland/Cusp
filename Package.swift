7b803ecfbf6d2180f50c19fd3d5a6f5cd43590a1
Daniel Clelland <daniel.clelland@gmail.com>
2025-02-27 19:51:49 +1300

    Set up SPM

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Cusp",
+    platforms: [
+        .macOS(.v13),
+        .iOS(.v14)
+    ],
+    products: [
+        .library(name: "Cusp", targets: ["Cusp"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/dclelland/Plinth", from: "2.7.1")
+    ],
+    targets: [
+        .target(
+            name: "Cusp",
+            dependencies: [
+                .product(name: "Plinth", package: "plinth")
+            ]
+        )
+    ]
+)
