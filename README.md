e90a05d12b3d286d0d1c73f4a722b609e973a7e5
Daniel Clelland <daniel.clelland@gmail.com>
2025-03-01 10:32:48 +1300

    x

--- README.md
+++ README.md
@@ -41,9 +41,8 @@ import Cusp
 - [x] Write 1D LCT implementation
 - [x] Write 2D LCT implementation
 - [x] Write LCT matrix generators
-    - Does this need arithmetic operations? Perhaps there is a generic 2x2 complex matrix type somewhere...
     
-- [ ] Figure out why the FrFT and LCT are not doing the same thing; check against canonical implementations
+- [x] Figure out why the FrFT and LCT are not doing the same thing; check against canonical implementations
 - [ ] What is the correct way to reconstruct the original image?
 - [ ] Add check in LCT for `b == 0`, as this causes a division by zero
 - [ ] Should the LCT support complex-valued matrices?
--- Sources/Cusp/Mathematics/Chirps.swift
+++ Sources/Cusp/Mathematics/Chirps.swift
@@ -14,7 +14,7 @@ extension ComplexMatrix where Scalar == Double {
         let alpha = order * .pi / 2.0
         
         let ramp = Matrix.centeredXRamp(shape: shape)
-        let factor = .pi / Scalar.tan(alpha) / Scalar(shape.columns)
+        let factor = .pi / tan(alpha) / Scalar(shape.columns)
         let phase = ramp.square() * factor
         
         return ComplexMatrix(real: phase.cos(), imaginary: phase.sin())
@@ -24,7 +24,7 @@ extension ComplexMatrix where Scalar == Double {
         let alpha = order * .pi / 2.0
         
         let ramp = Matrix.centeredXRamp(shape: shape)
-        let factor = .pi / Scalar.sin(alpha) / Scalar(shape.columns)
+        let factor = .pi / sin(alpha) / Scalar(shape.columns)
         let phase = ramp.square() * factor
         
         return ComplexMatrix(real: phase.cos(), imaginary: phase.sin())
@@ -36,7 +36,7 @@ extension ComplexMatrix where Scalar == Double {
     
     public static func lctInputChirp(shape: Shape, matrix: LCTMatrix<Scalar>) -> ComplexMatrix {
         let ramp = Matrix.centeredXRamp(shape: shape)
-        let factor = (matrix.a / (2.0 * matrix.b)) * (2.0 * .pi) / Scalar(shape.columns)
+        let factor = .pi * matrix.a / matrix.b / Scalar(shape.columns)
         let phase = ramp.square() * factor
         
         return ComplexMatrix(real: phase.cos(), imaginary: phase.sin())
@@ -44,7 +44,7 @@ extension ComplexMatrix where Scalar == Double {
     
     public static func lctOutputChirp(shape: Shape, matrix: LCTMatrix<Scalar>) -> ComplexMatrix {
         let ramp = Matrix.centeredXRamp(shape: shape)
-        let factor = (matrix.d / (2.0 * matrix.b)) * (2.0 * .pi) / Scalar(shape.columns)
+        let factor = .pi * matrix.d / matrix.b / Scalar(shape.columns)
         let phase = ramp.square() * factor
         
         return ComplexMatrix(real: phase.cos(), imaginary: phase.sin())
