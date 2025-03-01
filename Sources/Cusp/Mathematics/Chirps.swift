4fcd1c089a9cf219ed0a1c5d7be44272bbb052d5
Daniel Clelland <daniel.clelland@gmail.com>
2025-03-01 19:05:28 +1300

    Added check to avoid division by zero

--- Sources/Cusp/Mathematics/Chirps.swift
+++ Sources/Cusp/Mathematics/Chirps.swift
@@ -11,6 +11,10 @@ import Plinth
 extension ComplexMatrix where Scalar == Float {
     
     public static func lctPreChirp(shape: Shape, matrix: LCTMatrix<Scalar>) -> ComplexMatrix {
+        if matrix.b.isApproximatelyEqual(to: 0.0) {
+            return .ones(shape: shape)
+        }
+        
         let ramp = Matrix.centeredXRamp(shape: shape)
         let factor = .pi * matrix.a / matrix.b / Scalar(shape.columns)
         let phase = ramp.square() * factor
@@ -19,6 +23,10 @@ extension ComplexMatrix where Scalar == Float {
     }
     
     public static func lctPostChirp(shape: Shape, matrix: LCTMatrix<Scalar>) -> ComplexMatrix {
+        if matrix.b.isApproximatelyEqual(to: 0.0) {
+            return .ones(shape: shape)
+        }
+        
         let ramp = Matrix.centeredXRamp(shape: shape)
         let factor = .pi * matrix.d / matrix.b / Scalar(shape.columns)
         let phase = ramp.square() * factor
@@ -31,6 +39,10 @@ extension ComplexMatrix where Scalar == Float {
 extension ComplexMatrix where Scalar == Double {
     
     public static func lctPreChirp(shape: Shape, matrix: LCTMatrix<Scalar>) -> ComplexMatrix {
+        if matrix.b.isApproximatelyEqual(to: 0.0) {
+            return .ones(shape: shape)
+        }
+        
         let ramp = Matrix.centeredXRamp(shape: shape)
         let factor = .pi * matrix.a / matrix.b / Scalar(shape.columns)
         let phase = ramp.square() * factor
@@ -39,6 +51,10 @@ extension ComplexMatrix where Scalar == Double {
     }
     
     public static func lctPostChirp(shape: Shape, matrix: LCTMatrix<Scalar>) -> ComplexMatrix {
+        if matrix.b.isApproximatelyEqual(to: 0.0) {
+            return .ones(shape: shape)
+        }
+        
         let ramp = Matrix.centeredXRamp(shape: shape)
         let factor = .pi * matrix.d / matrix.b / Scalar(shape.columns)
         let phase = ramp.square() * factor
