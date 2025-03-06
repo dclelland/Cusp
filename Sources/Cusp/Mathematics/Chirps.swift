019791463ae7eec2299e954678137eead48c1ef6
Daniel Clelland <daniel.clelland@gmail.com>
2025-03-07 10:33:44 +1300

    Removed LCT-specific code

--- Sources/Cusp/Mathematics/Chirps.swift
+++ Sources/Cusp/Mathematics/Chirps.swift
@@ -28,34 +28,6 @@ extension ComplexMatrix where Scalar == Float {
     
 }
 
-extension ComplexMatrix where Scalar == Float {
-    
-    public static func lctPreChirp(shape: Shape, matrix: LCTMatrix<Scalar>) -> ComplexMatrix {
-        if matrix.b.isApproximatelyEqual(to: 0.0) {
-            return .ones(shape: shape)
-        }
-        
-        let ramp = Matrix.centeredXRamp(shape: shape)
-        let factor = .pi * matrix.a / matrix.b / Scalar(shape.columns)
-        let phase = ramp.square() * factor
-        
-        return ComplexMatrix(real: phase.cos(), imaginary: phase.sin())
-    }
-    
-    public static func lctPostChirp(shape: Shape, matrix: LCTMatrix<Scalar>) -> ComplexMatrix {
-        if matrix.b.isApproximatelyEqual(to: 0.0) {
-            return .ones(shape: shape)
-        }
-        
-        let ramp = Matrix.centeredXRamp(shape: shape)
-        let factor = .pi * matrix.d / matrix.b / Scalar(shape.columns)
-        let phase = ramp.square() * factor
-        
-        return ComplexMatrix(real: phase.cos(), imaginary: phase.sin())
-    }
-    
-}
-
 extension ComplexMatrix where Scalar == Double {
     
     public static func frftPreChirp(shape: Shape, order: Scalar) -> ComplexMatrix {
@@ -71,34 +43,6 @@ extension ComplexMatrix where Scalar == Double {
         let alpha = order * .pi / 2.0
         let factor = .pi / sin(alpha) / Scalar(shape.columns)
         let phase = ramp.square() * factor
-        return ComplexMatrix(real: phase.cos(), imaginary: phase.sin())
-    }
-    
-}
-
-extension ComplexMatrix where Scalar == Double {
-    
-    public static func lctPreChirp(shape: Shape, matrix: LCTMatrix<Scalar>) -> ComplexMatrix {
-        if matrix.b.isApproximatelyEqual(to: 0.0) {
-            return .ones(shape: shape)
-        }
-        
-        let ramp = Matrix.centeredXRamp(shape: shape)
-        let factor = .pi * matrix.a / matrix.b / Scalar(shape.columns)
-        let phase = ramp.square() * factor
-        
-        return ComplexMatrix(real: phase.cos(), imaginary: phase.sin())
-    }
-    
-    public static func lctPostChirp(shape: Shape, matrix: LCTMatrix<Scalar>) -> ComplexMatrix {
-        if matrix.b.isApproximatelyEqual(to: 0.0) {
-            return .ones(shape: shape)
-        }
-        
-        let ramp = Matrix.centeredXRamp(shape: shape)
-        let factor = .pi * matrix.d / matrix.b / Scalar(shape.columns)
-        let phase = ramp.square() * factor
-        
         return ComplexMatrix(real: phase.cos(), imaginary: phase.sin())
     }
     
